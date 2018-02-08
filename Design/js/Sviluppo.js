14cab90af3c090b3705bdb7b0f12d159108a3165
Dario Mastromarco <d.mastromarco@gmail.com>
2018-02-08 17:49:44 +0100

    animazioni
    navbar
    animazioni
    bonooooooo

---
+++ Design/js/Sviluppo.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    $('#logonav').mouseenter(function(){
+      var me =this;
+      this.style.webkitAnimation ='none';
+      setTimeout(function(){
+        me.style.webkitAnimation ='';
+      },10);
+    });
+    $('#smart').mouseenter(function(){
+      $('#smart').addClass('animated pulse');
+      var me =this;
+      this.style.webkitAnimation ='none';
+      setTimeout(function(){
+        me.style.webkitAnimation ='';
+      },10);
+    });
+    $('#pc').mouseenter(function(){
+      $('#pc').addClass('animated pulse');
+      var me =this;
+      this.style.webkitAnimation ='none';
+      setTimeout(function(){
+        me.style.webkitAnimation ='';
+      },10);
+    });
+    $('#camera').mouseenter(function(){
+      $('#camera').addClass('animated pulse');
+      var me =this;
+      this.style.webkitAnimation ='none';
+      setTimeout(function(){
+        me.style.webkitAnimation ='';
+      },10);
+    });
+    $('#cripto').mouseenter(function(){
+      $('#cripto').addClass('animated pulse');
+      var me =this;
+      this.style.webkitAnimation ='none';
+      setTimeout(function(){
+        me.style.webkitAnimation ='';
+      },10);
+    });
+    $('#hard').mouseenter(function(){
+      $('#hard').addClass('animated pulse');
+      var me =this;
+      this.style.webkitAnimation ='none';
+      setTimeout(function(){
+        me.style.webkitAnimation ='';
+      },10);
+    });
+    $('#droni').mouseenter(function(){
+      $('#droni').addClass('animated pulse');
+      var me =this;
+      this.style.webkitAnimation ='none';
+      setTimeout(function(){
+        me.style.webkitAnimation ='';
+      },10);
+    });
+    $('#logonav').addClass('animated pulse');
+});
