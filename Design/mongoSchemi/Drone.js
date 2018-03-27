40c96cb53ebe60168be8bf17b472fc093b925ece
DarioMastro95 <32960577+DarioMastro95@users.noreply.github.com>
2018-03-27 14:54:59 +0200

    drone schema

---
+++ Design/mongoSchemi/Drone.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://Mastronz:delphi1824@clustershopez-shard-00-00-xht2w.mongodb.net:27017,clustershopez-shard-00-01-xht2w.mongodb.net:27017,clustershopez-shard-00-02-xht2w.mongodb.net:27017/shopez?ssl=true&replicaSet=ClusterShopez-shard-0&authSource=admin');
+
+var droneSchema = new mongoose.Schema({
+  titolo:{type:String},
+  recensione:{type:String},
+  immagine:{type:String},
+  link:{type:String},
+  batteria:{type:String},
+  dimensione:{type:String},
+  distanza:{type:String},
+  autonomia:{type:String},
+  processore:{type:String},
+  controller:{type:String},
+  risoluzione:{type:String},
+  pro:[String],
+  contro:[String],
+  position:{type:Number},
+  fascia:{type:String}
+});
+
+var Tablet = mongoose.model('drone',droneSchema);
+module.exports= Tablet;
--- Design/public/js/dashboard.js
+++ Design/public/js/dashboard.js
@@ -1,80 +1,120 @@
 $(document).ready(function() {
-  $('#buttonLoadArt').click(function(event){
-    event.preventDefault();
-    var categoria = $('#Categoria').val();
-    console.log(categoria);
-    if(categoria=='Smartphone'){
-      var smartphone = {
-          titolo:$('#titolo').val(),
-          recensione:$('#recensione').val(),
-          immagine:$('#immagine').val(),
-          link:$('#link').val(),
-          batteria:$('#batteria').val(),
-          fotocamera:$('#fotocamera').val(),
-          display:$('#display').val(),
-          memoria:$('#memoria').val(),
-          processore:$('#processore').val(),
-          ram:$('#ram').val(),
-          rete:$('#rete').val(),
-          so:$('#so').val(),
-          pro:$('#pro').val().split(','),
-          contro:$('#contro').val().split(','),
-          position:$('#position').val(),
-          fascia:$('#fascia').val()
-        }
-      $.post(
-        '/smartphone',
-        smartphone,
-        function(response){
-          if(response.success){
-            alert('Smartphone '+smartphone.titolo+' inserito correttamente');
-          }
-          else{
-            alert(response.extra);
-          }
-        }
-      );
+  $('#Categoria').change(function(){
+    if($('#Categoria').val()=='Smartphone'){
+      $('#buttonLoadArt').click(function(event){
+        event.preventDefault();
+          var smartphone = {
+              titolo:$('#titolo').val(),
+              recensione:$('#recensione').val(),
+              immagine:$('#immagine').val(),
+              link:$('#link').val(),
+              batteria:$('#batteria').val(),
+              fotocamera:$('#fotocamera').val(),
+              display:$('#display').val(),
+              memoria:$('#memoria').val(),
+              processore:$('#processore').val(),
+              ram:$('#ram').val(),
+              rete:$('#rete').val(),
+              so:$('#so').val(),
+              pro:$('#pro').val().split(','),
+              contro:$('#contro').val().split(','),
+              position:$('#position').val(),
+              fascia:$('#fascia').val()
+            }
+          $.post(
+            '/smartphone',
+            tablet,
+            function(response){
+              if(response.success){
+                alert('Smartphone '+smartphone.titolo+' inserito correttamente');
+              }
+              else{
+                alert(response.extra);
+              }
+            });
+          );
+        $('.formDashboard').each(function(index,element) {
+          element.reset();
+        });
+      });
     }
-    else if(categoria=='Tablet'){
-      var tablet = {
-          titolo:$('#titolo').val(),
-          recensione:$('#recensione').val(),
-          immagine:$('#immagine').val(),
-          link:$('#link').val(),
-          batteria:$('#batteria').val(),
-          fotocamera:$('#fotocamera').val(),
-          display:$('#display').val(),
-          memoria:$('#memoria').val(),
-          processore:$('#processore').val(),
-          ram:$('#ram').val(),
-          rete:$('#rete').val(),
-          so:$('#so').val(),
-          pro:$('#pro').val().split(','),
-          contro:$('#contro').val().split(','),
-          position:$('#position').val(),
-          fascia:$('#fascia').val()
-        }
-      $.post(
-        '/tablet',
-        tablet,
-        function(response){
-          if(response.success){
-            alert('Tablet '+tablet.titolo+' inserito correttamente');
-          }
-          else{
-            alert(response.extra);
-          }
-        }
-      );
+    else if($('#Categoria').val()=='Tablet'){
+      $('#buttonLoadArt').click(function(event){
+        event.preventDefault();
+          var tablet = {
+              titolo:$('#titolo').val(),
+              recensione:$('#recensione').val(),
+              immagine:$('#immagine').val(),
+              link:$('#link').val(),
+              batteria:$('#batteria').val(),
+              fotocamera:$('#fotocamera').val(),
+              display:$('#display').val(),
+              memoria:$('#memoria').val(),
+              processore:$('#processore').val(),
+              ram:$('#ram').val(),
+              rete:$('#rete').val(),
+              so:$('#so').val(),
+              pro:$('#pro').val().split(','),
+              contro:$('#contro').val().split(','),
+              position:$('#position').val(),
+              fascia:$('#fascia').val()
+            }
+          $.post(
+            '/tablet',
+            tablet,
+            function(response){
+              if(response.success){
+                alert('Tablet '+tablet.titolo+' inserito correttamente');
+              }
+              else{
+                alert(response.extra);
+              }
+            });
+          );
+        $('.formDashboard').each(function(index,element) {
+          element.reset();
+        });
+      });
     }
-
-    $('.formDashboard').each(function(index,element) {
-      element.reset();
-    })
+    else if($('#Categoria').val()=='Drone'){
+      $('#buttonLoadArt').click(function(event){
+        event.preventDefault();
+        var drone = {
+            titolo:$('#titolo').val(),
+            recensione:$('#recensione').val(),
+            immagine:$('#immagine').val(),
+            link:$('#link').val(),
+            batteria:$('#batteria').val(),
+            fotocamera:$('#fotocamera').val(),
+            display:$('#display').val(),
+            memoria:$('#memoria').val(),
+            processore:$('#processore').val(),
+            ram:$('#ram').val(),
+            rete:$('#rete').val(),
+            so:$('#so').val(),
+            pro:$('#pro').val().split(','),
+            contro:$('#contro').val().split(','),
+            position:$('#position').val(),
+            fascia:$('#fascia').val()
+          }
+        $.post(
+          '/drone',
+          tablet,
+          function(response){
+            if(response.success){
+              alert('Drone '+drone.titolo+' inserito correttamente');
+            }
+            else{
+              alert(response.extra);
+            }
+          }
+        );
+        $('.formDashboard').each(function(index,element) {
+          element.reset();
+        });
+      });
+    }
   });
-
-
-
 
   $('#bold').click(function(event) {
     $('#recensione').val($('#recensione').val()+('<b></b>'));
