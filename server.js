3bbe988224ae23bc4f4ca2e2083f99b05f5b2b45
DarioMastro95 <32960577+DarioMastro95@users.noreply.github.com>
2018-04-23 09:48:49 +0200

    sitemap

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT||3000;
 const bodyParser = require('body-parser');
-
+const SitemapGenerator = require('sitemap-generator');
 var routes = require('./routes/routes.js');
 var session = require('express-session');
 var Smartphone = require('./mongoSchemi/Smartphone');
@@ -14,6 +14,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({ secret: 'jskdjsdksjdksjdkksdj', resave: false, saveUninitilized: true }));
 app.use('/', routes);
 app.set('view engine', 'ejs');
+// create generator
+const generator = SitemapGenerator('https://www.shopezhub.com/', {
+  stripQuerystring: false
+});
+
+// register event listeners
+generator.on('done', () => {
+  // sitemaps created
+});
 app.listen(port, function () {
   console.log(port);
+  generator.start();
 });
