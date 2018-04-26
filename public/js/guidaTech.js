6aa622073f953ed3a0c0e51bb109eb79c984d9f6
DarioMastro95 <32960577+DarioMastro95@users.noreply.github.com>
2018-04-26 09:33:10 +0200

    fix link guida

--- public/js/guidaTech.js
+++ public/js/guidaTech.js
@@ -16,12 +16,11 @@ $(document).ready(function() {
           $('#contenuto').append('<div class="row" id="'+i+'"><div class="col-lg-4 col-md-12"><div class="row"><div class="subtitolo"><h5 class="titolo"></h5><hr></div></div><div class="row"><div class="picture"><img class="img-fluid imgart immagine" src="" alt=""></div></div><div class="row"><div class="acquista"><a class="button link" href="" onclick="window.open(this.href);return false" role="button"><span><b>ACQUISTA</b></span><div class="icon"><i class=""><img id="amazon" class="img-fluid"src="/img/icone/amazon-icon.svg" width="50px" height="50px"></i><i class="fa fa-check"></i></div></a></div></div></div><div class="col-lg-8"><div class="row"><div class="subtitolo"><h5>Recensione</h5><hr></div><div class="recensione pre-scrollable"></div></div><div class="row"><div class="col-lg-6 col-md-6 near"><div class="pro"><b>PRO</b></div><div class="proContent"><ul class="proUl proDin"></ul></div></div><div class="col-lg-6 col-md-6 near"><div class="contro"><b>CONTRO</b></div><div class="controContent"><ul class="controUl controDin"></ul></div></div></div></div></div>');
           lPro = guidaT.guida.prodotti[i].pro.length;
           lContro = guidaT.guida.prodotti[i].contro.length;
-          console.log(introduzione);
           $('#'+i).find('.recensione').append(guidaT.guida.prodotti[i].descrizione);
           $('#'+i).find('.titolo').append(guidaT.guida.prodotti[i].nome);
           $('#'+i).find('.immagine').attr('src',guidaT.guida.prodotti[i].immagine);
           $('#'+i).find('.immagine').attr('alt',guidaT.guida.prodotti[i].nome);
-          $('#'+i).find('.link').attr('href',guidaT.guida.prodotti[i].linkAmazon);
+          $('#'+i).find('.link').attr('href',guidaT.guida.prodotti[i].link);
           if(guidaT.guida.prodotti[i].pro[0]!=''){
             for (var j = 0; j < lPro ; j++) {
               $('#'+i).find('.proDin').append('<li>'+guidaT.guida.prodotti[i].pro[j]+'</li>');
