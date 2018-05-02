75a875ef9d7340414243bb1ffb22a4270b18ce76
DarioMastro95 <32960577+DarioMastro95@users.noreply.github.com>
2018-05-02 17:30:00 +0200

    update server

--- server.js
+++ server.js
@@ -7,20 +7,16 @@ var routes = require('./routes/routes.js');
 var session = require('express-session');
 var Smartphone = require('./mongoSchemi/Smartphone');
 var User = require('./mongoSchemi/User');
-
+var compression = require('compression');
+var secure = require('express-force-https');
 app.use(express.static(__dirname + '/public'));
+app.use(secure);
 app.use(bodyParser.json());
+app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({ secret: 'jskdjsdksjdksjdkksdj', resave: false, saveUninitilized: true }));
 app.use('/', routes);
 app.set('view engine', 'ejs');
-app.use(function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
-    const secureUrl = 'https://' + req.hostname + req.originalUrl
-    res.redirect(302, secureUrl)
-  }
-  next();
-});
 // create generator
 const generator = SitemapGenerator('https://www.shopezhub.com/', {
   stripQuerystring: false
